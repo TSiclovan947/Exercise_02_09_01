a30d48bc9ff7b24098cdbd88359db68f074c3599
Unknown <tsiclovan947@west-mec.org>
2018-11-28 17:25:30 -0800

    Request Opportunities and Cookies
    Caught up on missing work; Worked with and created/used a persistent cookie; created a login with available opportunities

--- AvailableOpportunities.php
+++ AvailableOpportunities.php
@@ -1,18 +1,17 @@
 <!doctype html>
-
 <html>
 
 <head>
-   <!--   
+<!--   
          Exercise 02_09_01
          Author: Tabitha Siclovan
-         Date: November 15, 2018
+         Date: November 28, 2018
         
          AvailableOpportunities.php
     -->
     <title>Available Opportunities</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="initial-scale=1.0">
+    <meta name="viewport" content="inital-scale=1.0">
     <script src="modernizr.custom.65897.js"></script>
 </head>
 
@@ -20,42 +19,44 @@
     <h1>College Internship</h1>
     <h2>Available Opportunities</h2>
     <?php
-    //Request used when various pages (get/post does not matter)
     if (isset($_REQUEST['internID'])) {
         $internID = $_REQUEST['internID'];
     }
     else {
-       $internID = -1;
+        $internID = -1;
+    }
+    if (isset($_COOKIE['LastRequestDate'])) {
+        $lastRequestDate = urldecode($_COOKIE['LastRequestDate']);
+    }
+    else {
+        $lastRequestDate = "";
     }
     //debug
     echo "\$internID: $internID\n";
-     //Errors variable to control the flow
+    
     $errors = 0;
-    //Connect to database variable needed
     $hostname = "localhost";
     $username = "adminer";
     $passwd = "after-water-49";
     $DBConnect = false;
     $DBName = "internships2";
-    //Opening database connection
-    //Make it scallable, maintainability
     if ($errors == 0) {
-        //Only do a connection if generated no errors in validation
-        $DBConnect = mysqli_connect($hostname, $username, $passwd);
+        $DBConnect = mysqli_connect($hostname, $username,
+        $passwd);
         if (!$DBConnect) {
             ++$errors;
             echo "<p>Unable to connect to the database server" .
-                " error code: " . mysqli_connect_error() . "</p>\n";
+                " error code: " . mysqli_connect_error() . 
+                "</p>\n";
         }
-        //Select the database
         else {
-            $result =  mysqli_select_db($DBConnect, $DBName);
+            $result = mysqli_select_db($DBConnect, $DBName);
             if (!$result) {
-                //generates error if cannot select database
-                ++$errors;
-                echo "<p>Unable to select the database" .
+            ++$errors;
+            echo "<p>Unable to select the database" .
                 " \"$DBName\" error code: " . 
-                mysqli_error($DBConnect) . "</p>\n";
+                mysqli_error($DBConnect) . 
+                "</p>\n";               
             }
         }
     }
@@ -66,16 +67,126 @@
         $queryResult = mysqli_query($DBConnect, $SQLstring);
         if (!$queryResult) {
             ++$errors;
-            echo "<p>Unable to execute the query, error code: " . 
-                mysqli_errno($DBConnect) . ": " . 
+            echo "<p>Unable to execute the query, error code: "
+                . mysqli_errno($DBConnect) . ": " . 
                 mysqli_error($DBConnect) . "</p>\n";
         }
+        else {
+            if (mysqli_num_rows($queryResult) == 0) {
+                ++$errors;
+                echo "<p>Invalid Intern ID!</p>\n";
+            }
+        }
     }
-    //Close database connection
+    if ($errors == 0) {
+        $row = mysqli_fetch_assoc($queryResult);
+        $internName = $row['first'] . " " . $row['last'];
+    }
+    else {
+        $internName = "";
+    }
+    // debug
+    echo "\$internName: $internName";
+    $TableName = "assigned_opportunities";
+    if ($errors == 0) {
+        $SQLstring = "SELECT count(opportunityID)" .
+            " FROM $TableName" . 
+            " WHERE internID='$internID'" . 
+            " AND dateApproved IS NOT NULL";
+        $queryResult = mysqli_query($DBConnect, $SQLstring);
+        if (mysqli_num_rows($queryResult) > 0) {
+            $row = mysqli_fetch_row($queryResult);
+            $approvedOpportunities = $row[0];
+            mysqli_free_result($queryResult);
+        }
+    }
+    if ($errors == 0) {
+        $selectedOpportunities = array();
+        $SQLstring = "SELECT opportunityID FROM $TableName" . 
+            " WHERE internID='$internID'";
+        $queryResult = mysqli_query($DBConnect, $SQLstring);
+        if (mysqli_num_rows($queryResult) > 0) {
+            while (($row = mysqli_fetch_row($queryResult)) != 
+            false) {
+                $selectedOpportunities[] = $row[0];
+            }
+            mysqli_free_result($queryResult);
+        }
+        $assignedOpportunities = array();
+        $SQLstring = "SELECT opportunityID FROM $TableName" . 
+            " WHERE dateApproved IS NOT NULL";
+        $queryResult = mysqli_query($DBConnect, $SQLstring);
+        if (mysqli_num_rows($queryResult) > 0) {
+            while (($row = mysqli_fetch_row($queryResult)) != 
+            false) {
+                $assignedOpportunities[] = $row[0];
+            }
+            mysqli_free_result($queryResult);
+        }
+    }
+    $TableName = "opportunities";
+    $opportunities = array();
+    if ($errors == 0) {
+        $SQLstring = "SELECT opportunityID, company, city," . 
+            " startDate, endDate, position, description" . 
+            " FROM $TableName";
+        $queryResult = mysqli_query($DBConnect, $SQLstring);
+        if (mysqli_num_rows($queryResult) > 0) {
+            while (($row = mysqli_fetch_assoc($queryResult)) != 
+            false) {
+                $opportunities[] = $row;
+            }
+            mysqli_free_result($queryResult);
+        }
+    }
     if ($DBConnect) {
         echo "<p>Closing database connection.</p>\n";
-        mysqli_close($DBConnect);
+        mysqli_close($DBConnect);     
     }
+    if (!empty($lastRequestDate)) {
+        echo "<p>You last requested an internship" .
+            " opportunity on $lastRequestDate.</p>\n";
+    }
+    echo "<table border='1' width='100%'>\n";
+    echo "<tr>\n";
+    echo "<th style='background-color: cyan'>Company</th>\n";
+    echo "<th style='background-color: cyan'>City</th>\n";
+    echo "<th style='background-color: cyan'>Start Date</th>\n";
+    echo "<th style='background-color: cyan'>End Date</th>\n";
+    echo "<th style='background-color: cyan'>Position</th>\n";
+    echo "<th style='background-color: cyan'>Description</th>\n";
+    echo "<th style='background-color: cyan'>Status</th>\n";
+    echo "</tr>\n";
+    foreach ($opportunities as $opportunity) {
+        if (!in_array($opportunity['opportunityID'], $assignedOpportunities)) {
+            echo "<tr>\n";
+            echo "<td>" . htmlentities($opportunity['company']) . "</td>\n";
+            echo "<td>" . htmlentities($opportunity['city']) . "</td>\n";
+            echo "<td>" . htmlentities($opportunity['startDate']) . "</td>\n";
+            echo "<td>" . htmlentities($opportunity['endDate']) . "</td>\n";
+            echo "<td>" . htmlentities($opportunity['position']) . "</td>\n";
+            echo "<td>" . htmlentities($opportunity['description']) . "</td>\n";
+            echo "<td>";
+            if (in_array($opportunity['opportunityID'], 
+            $selectedOpportunities)) {
+                echo "Selected";
+            }
+            else if ($approvedOpportunities) {
+                echo "Open";
+            }
+            else {
+                echo "<a href ='RequestOpportunity.php?" . 
+                    "internID=$internID&" . 
+                    "opportunityID=" . 
+                    $opportunity['opportunityID'] . 
+                    "'>Available</a>";
+            }
+            echo "</td>";
+            echo "</tr>\n";
+        }
+    }
+    echo "</table>\n";
+    echo "<p><a href='InternLogin.php'>Log Out</a></p>\n";
     ?>
 </body>
 
